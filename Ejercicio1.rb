30f5637b0bb3b1378a1abb7db4b13c1cfc744959
Catdmio <montecinos.catalina@gmail.com>
2017-09-04 20:13:46 -0300

    this method receives an array

--- Ejercicio1.rb
+++ Ejercicio1.rb
@@ -11,4 +11,20 @@ def create_index(p1, p2)
   file.close
 end
 
+
+
+#Crear un método similar al anterior, que además pueda recibir
+#un arreglo. Si el arreglo no está vacío, agregar debajo de los
+#párrafos una lista ordenada con cada uno de los elementos.
+def add_list(list)
+  return if list.count.zero?
+  file = File.open('index.html', 'a')
+  file.puts '<ol>'
+  list.each { |ele| puts "<li> #{ele} </li>" }
+  file.puts '</ol>'
+  file.close
+end
+
 create_index('Hola', 'Inmundo')
+ordered_list= %w[ele1 ele2 ele3 ele4]
+add_list(ordered_list)
